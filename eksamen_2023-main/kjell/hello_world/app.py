8964b4ccd447412ad96ddbe2ab5d0c4ec865f3fc
thsletten <sletten.tom@gmail.com>
2023-11-22 22:58:00 +0000

    meg dumme meg glemte å commite

--- eksamen_2023-main/kjell/hello_world/app.py
+++ eksamen_2023-main/kjell/hello_world/app.py
@@ -2,21 +2,11 @@ import json
 import boto3
 import os
 
-# Denne koden kan også kjøres som en selvstendig applikasjon (Uten SAM) bare gjøre følgende
-# (dersom man har python på maskinen sin altså...)
-#
-# Instruksjoner for å kjøre ... (Kan sikkert lage container senere ..)
-#
-# pip3 install -r requirements.txt
-# python3 app.py
-#
-# Hilsen Kjell
-
 s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
-# Oppgave 1A
-BUCKET_NAME = "kjellsimagebucker"
+# Get the bucket name from the environment variable
+BUCKET_NAME = os.environ['BUCKET_NAME']
 
 def lambda_handler(event, context):
 
@@ -46,4 +36,3 @@ def lambda_handler(event, context):
         "body":  json.dumps(rekognition_results),
     }
 
-print(lambda_handler(None, None))
